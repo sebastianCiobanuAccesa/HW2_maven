1067b8bd9007b3106cf52a9efa5460bba16e01b9
sebastianCiobanuAccesa <sebastian.ciobanu@accesa.eu>
2022-07-22 16:07:09 +0300

    Done

--- code/example/Main.java
+++ code/example/Main.java
@@ -3,7 +3,8 @@ package example;
 
 public class Main {
     public static void main(String[] args) {
-
+        // "Install and check your built jar in your maven local repository (.m2).":
+        // C:\Users\sebastian.ciobanu\.m2\repository\org\example\HW2_maven\1.0-SNAPSHOT\
         System.out.println("Hello world!");
     }
 }
